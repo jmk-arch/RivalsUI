be33d83ff18383af8b8bb2f9492865f9c3520065
west <joinewith@gmail.com>
2026-06-08 19:23:23 +0900

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -283,46 +283,7 @@ local SaveManager = {} do
 			SaveManager.AutoloadLabel:SetText('Current autoload config: ' .. name)
 		end
 
-		local communitySection = tab:AddRightGroupbox('community configs')
-		
-		communitySection:AddDropdown('SaveManager_CommunityConfigList', { 
-			Text = 'config list', 
-			Values = {'[LEGIT] Rem`s Config', '[RAGE] Rem`s Config', '[Legit] Magma', '[LEGIT] Winter'}, 
-			AllowNull = true 
-		})
-
-		communitySection:AddButton('load config', function()
-			local name = Options.SaveManager_CommunityConfigList.Value
-			if not name then return end
-
-			local communityConfigsData
-			pcall(function()
-				if isfile("addons/CommunityConfigsData.lua") then
-					communityConfigsData = loadstring(readfile("addons/CommunityConfigsData.lua"))()
-				end
-			end)
-
-			if communityConfigsData and communityConfigsData[name] then
-				local decoded = communityConfigsData[name]
-				for _, option in next, decoded.objects do
-					if self.Parser[option.type] then
-						task.spawn(function() self.Parser[option.type].Load(option.idx, option) end)
-					end
-				end
-				if decoded.custom and self.CustomLoad then
-					task.spawn(function() self.CustomLoad(decoded.custom) end)
-				end
-				self.Library:Notify(string.format('Loaded community config %q', name))
-			else
-				local success, err = self:Load(name)
-				if not success then
-					return self.Library:Notify('Failed to load config: ' .. tostring(err))
-				end
-				self.Library:Notify(string.format('Loaded config %q', name))
-			end
-		end)
-
-		SaveManager:SetIgnoreIndexes({ 'SaveManager_ConfigList', 'SaveManager_ConfigName', 'SaveManager_CommunityConfigList' })
+		SaveManager:SetIgnoreIndexes({ 'SaveManager_ConfigList', 'SaveManager_ConfigName' })
 	end
 
 	SaveManager:BuildFolderTree()
